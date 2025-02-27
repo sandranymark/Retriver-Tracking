0a06a83abf801efcd88664b292b55695f93d182d
sandranymark <sandra@nymark.nu>
2025-02-27 15:32:04 +0100

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: AppComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'auth', component: AuthComponent },
     { path: 'profile', component: ProfileComponent },
     { path: '**', redirectTo: '' }
